84f269dbf7e8c7b710d2a7e61701d5d73c5c2d8e
JMarcu <james.marcu@yahoo.com>
2021-05-16 19:54:18 -0700

    Simple execution.

--- src/main/java/models/ServerMessage/MessageExecutor.java
+++ src/main/java/models/ServerMessage/MessageExecutor.java
@@ -11,30 +11,30 @@ public class MessageExecutor extends Thread{
     private LinkedBlockingQueue<Runnable> queue; 
     
     private MessageExecutor(){
-        executor = Executors.newFixedThreadPool(20);
         queue = new LinkedBlockingQueue<Runnable>();
+        executor = Executors.newFixedThreadPool(20);
     }
 
     public void queueMessageHandler(Runnable handler) throws InterruptedException{
         System.out.println("Queueing Handler");
-        queue.put(handler);
-        System.out.println("Queue Is Empty: " + queue.isEmpty());
+        // queue.put(handler);
+        executor.execute(handler);
     }
     
     public void run() {
-        System.out.println("Beginning Executor");
+    //     System.out.println("Beginning Executor");
 
-        while(true){
-            if(!queue.isEmpty()){
-                System.out.println("Queue Is Not Empty");
-                try {
-                    executor.execute(queue.take());
-                } catch (Exception e) {
-                    System.out.println("Error executing handler :: " + e);
-                    e.printStackTrace();
-                }
-            }
-       }
+    //     while(true){
+    //         if(!queue.isEmpty()){
+    //             System.out.println("Queue Is Not Empty");
+    //             try {
+    //                 executor.execute(queue.take());
+    //             } catch (Exception e) {
+    //                 System.out.println("Error executing handler :: " + e);
+    //                 e.printStackTrace();
+    //             }
+    //         }
+    //    }
     }
 
    /** 
