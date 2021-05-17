e9b4fc0a8d15cc460442b6a2a299459db7ad1e63
JMarcu <james.marcu@yahoo.com>
2021-05-16 21:46:15 -0700

    Logging for getRefreshToken

--- src/main/java/models/ServerMessage/LogoutMessageBody.java
+++ src/main/java/models/ServerMessage/LogoutMessageBody.java
@@ -3,14 +3,14 @@ package models.ServerMessage;
 import java.util.UUID;
 
 public class LogoutMessageBody {
-    private String refreshToken;
     private UUID playerId;
+    private String refreshToken;
 
     public LogoutMessageBody(String refreshToken, UUID playerId){
-        this.refreshToken = refreshToken;
         this.playerId = playerId;
+        this.refreshToken = refreshToken;
     }
 
+    public UUID getPlayerId(){return playerId; }
     public String getRefreshToken(){ return refreshToken; }
-    public UUID getPlayerId(){return playerId;}
 }
